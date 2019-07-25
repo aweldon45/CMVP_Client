08e71b1fdac95871259b97582091933a9216526c
Tony <anthony.weldon.jr@gmail.com>
2019-07-25 05:26:37 -0400

    first user commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Search from './Search';
+import { Layout }  from './components/Layout';
+import { Navigationbar } from './components/Navigationbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+      <Navigationbar/>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component = {Home} />
+            <Route path="/Login" component = {Login} />
+            <Route path="/Search" component = {Search} />
+          </Switch>
+        </Router>
+      </Layout>
+      </React.Fragment>
+    )};
 }
 
 export default App;
---
+++ src/components/Navigationbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+.navbar {
+  background-color: #0C1A30;
+}
+
+.navbar-brand, .navbar-nav .nav-link, .navbar-toggle {
+  color: #FF72BD;
+
+&: hover {
+  color: white;
+}
+
+}`
+
+export const Navigationbar = (props) => (
+    <Styles>
+      <Navbar expand="lg">
+          <Navbar.Brand href="/">Collabeteria</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/Login">Login</Nav.Link>
+            <Nav.Link href="/Search">Find Collaborators</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles>
+)
