f6ff53481af693995debf88b3b65e74cc8660138
Tony <anthony.weldon.jr@gmail.com>
2019-08-24 13:32:12 -0400

    first search implementation

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
-//import styled from 'styled-components';
 
 //Add a project
 //name
--- src/layouts/Search.js
+++ src/layouts/Search.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import { SearchCard } from '../components/SearchCard'
+import { SearchCard } from '../components/SearchCard';
+import { Form } from 'react-bootstrap'
 
 class Search extends Component {
+  constructor () {
+    super();
+    this.state = {
+      filter1: ''
+    }
+  }
+
+  updatefilter(event) {
+    this.setState({filter1: event.target.value})
+  }
+
   render() {
-    return ( this.props.projects.map((project) => (
-      <SearchCard title={project.title} role={project.director}>
-        </SearchCard>
-    ))
-    )
+      const filteredRoles = this.props.projects.filter((project) => {
+        return project.director.toLowerCase().indexOf(this.state.filter1.toLowerCase()) !== -1;
+      })
+
+    const results = filteredRoles.map((project) => (
+    <SearchCard title={project.title} role={project.director} key={project.id}>
+      </SearchCard>))
+
+      return (
+        <div>
+        <Form>
+          <Form.Group controlId='FilterForm'>
+            <Form.Label></Form.Label>
+            <Form.Control inputtype='text' value={this.state.filter1} onChange={this.updatefilter.bind(this)}/>
+            <Form.Text></Form.Text>
+          </Form.Group>
+        </Form>
+        {[ results ]}
+        </div>
+      )
   }
 }
 
